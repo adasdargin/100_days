e6830a9d3b5565ec630f4652de399e0d9a555cbe
Adas Dargin <adasdargin@Adass-Mac-mini.lan>
2021-04-03 20:03:10 +0300

    day 16 update

--- days_intermediate_15-20/16_object_oriented_programming/oop_coffee_machine/program.py
+++ days_intermediate_15-20/16_object_oriented_programming/oop_coffee_machine/program.py
@@ -1,4 +1,4 @@
-from menu import Menu, MenuItem
+from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
@@ -22,5 +22,3 @@ while is_on:
         is_payment_successful = money_machine.make_payment(drink.cost)
         if is_enough_ingredients and is_payment_successful:
             coffee_maker.make_coffee(drink)
-
-
